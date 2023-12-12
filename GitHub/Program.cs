f30b01005590148c47a62a4cd0bbb0243f8360ab
L2466 <L2466@LX4PC11>
2023-12-12 11:49:49 +0000

    Adicionar arquivos de projeto.

---
+++ GitHub/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, resultado;
+
+            Console.WriteLine("Escolha um numero para A");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escolha um numero para B");
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+          resultado = a + b;
+
+            Console.WriteLine("O resultado é");
+
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+
+
+    }
+}
